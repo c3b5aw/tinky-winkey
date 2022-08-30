03d3fe7ffe6a85c03648bc11e545546df6b9c9db
c3b5aw <dev@c3b5aw.dev>
2022-08-30 14:19:28 -0700

    feat: all service command

--- src/svc.cpp
+++ src/svc.cpp
@@ -6,6 +6,8 @@ void Service::Install() {
 	SC_HANDLE schService;
 	TCHAR szPath[MAX_PATH + 2] = {0};
 
+	std::cout << "Install\n";
+
 	if (!GetModuleFileName(NULL, szPath + 1, MAX_PATH)) {
 		std::cout << std::format("Cannot install service({})\n", GetLastError());
 		return;
@@ -36,7 +38,13 @@ void Service::Install() {
 	);
 
 	if (schService == nullptr) {
-		std::cout << std::format("CreateServiec failed ({})\n", GetLastError());
+		const DWORD lastError = GetLastError();
+		if (lastError == 1073) {
+			std::cout << "Service is already installed";
+		}
+		else {
+			std::cout << std::format("CreateService failed ({})\n", GetLastError());
+		}
 		CloseServiceHandle(schSCManager);
 		return;
 	}
@@ -45,11 +53,107 @@ void Service::Install() {
 	CloseServiceHandle(schSCManager);
 }
 
-void Service::Start() {}
+void Service::Start() {
+	auto [schSCManager, schService] = getHandlers();
+	if (schSCManager == nullptr || schService == nullptr) {
+		return;
+	}
 
-void Service::Stop() {}
+	std::cout << "Starting service\n";
+	if (StartService(schService, 0, NULL)) {
+		std::cout << "Service started succesfully\n";
+	}
+	else {
+		const DWORD lastError = GetLastError();
+		if (lastError == 1056) {
+			std::cout << "Service already started\n";
+		}
+		else {
+			std::cout << std::format("Service failed to start ({})\n", lastError);
+		}
+	}
 
-void Service::Delete() {}
+	CloseServiceHandle(schService);
+	CloseServiceHandle(schSCManager);
+}
+
+std::tuple<SC_HANDLE, SC_HANDLE> Service::getHandlers() {
+	SC_HANDLE schService;
+	SC_HANDLE schSCManager;
+
+	std::cout << "getHandlers\n";
+
+	schSCManager = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
+	if (schSCManager == nullptr) {
+		std::cout << std::format("OpenSCManager failed ({})\n", GetLastError());
+		return std::tuple(nullptr, nullptr);
+	}
+
+	schService = OpenService(
+		schSCManager,
+		SVCNAME,
+		SERVICE_ALL_ACCESS
+	);
+	if (schService == nullptr) {
+		std::cout << std::format("OpenService failed ({})\n", GetLastError());
+		CloseServiceHandle(schSCManager);
+		return std::tuple(nullptr, nullptr);
+	}
+	return std::make_tuple(schSCManager, schService);
+}
+
+void Service::Stop() {
+	auto [schSCManager, schService] = getHandlers();
+	if (schSCManager == nullptr || schService == nullptr) {
+		return;
+	}
+
+	std::cout << "Stopping service\n";
+	if (ControlService(schService, SERVICE_CONTROL_STOP, &gSvcStatus)) {
+		std::cout << "Service stopped successfully\n";
+	}
+	else {
+		const DWORD lastError = GetLastError();
+		if (lastError == 1062) {
+			std::cout << "Service already stopped\n";
+		}
+		else {
+			std::cout << std::format("ControlService STOP failed ({})\n", lastError);
+		}
+	}
+
+	CloseServiceHandle(schService);
+	CloseServiceHandle(schSCManager);
+}
+
+void Service::Delete() {
+	auto [schSCManager, schService] = getHandlers();
+	if (schSCManager == nullptr || schService == nullptr) {
+		return;
+	}
+
+	if (ControlService(schService, SERVICE_CONTROL_STOP, &gSvcStatus)) {
+		std::cout << "Stopping service\n";
+		while (QueryServiceStatus(schService, &gSvcStatus)) {
+			if (gSvcStatus.dwCurrentState == SERVICE_STOP_PENDING) {
+				std::cout << ".";
+				std::this_thread::sleep_for(std::chrono::milliseconds(250));
+			}
+			else {
+				break;
+			}
+		}
+	}
+
+	if (DeleteService(schService)) {
+		std::cout << "Service has been removed\n";
+	}
+	else {
+		std::cout << std::format("DeleteService failed ({})\n", GetLastError());
+	}
+	CloseServiceHandle(schService);
+	CloseServiceHandle(schSCManager);
+}
 
 /*
 	__cdecl__: Stack is cleanup by the caller
@@ -58,18 +162,17 @@ void Service::Delete() {}
 int __cdecl _tmain(int argc, TCHAR* argv[])
 {
 	if (argc == 2) {
-		if (std::strcmp(argv[1], "install")) {
+		if (std::strcmp(argv[1], "install") == 0) {
 			Service::Install();
 		}
-		else if (std::strcmp(argv[1], "start")) {
+		else if (std::strcmp(argv[1], "start") == 0) {
 			Service::Start();
 		}
-		else if (std::strcmp(argv[1], "stop")) {
+		else if (std::strcmp(argv[1], "stop") == 0) {
 			Service::Stop();
 		}
-		else if (std::strcmp(argv[1], " delete")) {
+		else if (std::strcmp(argv[1], "delete") == 0) {
 			Service::Delete();
 		}
 	}
-	std::cout << __cplusplus << std::endl;
 }
--- src/svc.h
+++ src/svc.h
@@ -5,14 +5,22 @@
 #include <tchar.h>
 #include <strsafe.h>
 #include <cstring>
+#include <thread>
+#include <chrono>
 #pragma comment(lib, "advapi32.lib")
 
 #define SVCNAME TEXT("tinky")
+
+SERVICE_STATUS gSvcStatus;
+SERVICE_STATUS_HANDLE gSvcStatusHandle;
+HANDLE ghSvcStopEvent = NULL;
 
 /*
 	Docs: https://docs.microsoft.com/en-us/windows/win32/services/svc-cpp
 */
 class Service {
+private:
+	static std::tuple<SC_HANDLE, SC_HANDLE> _getHandlers();
 public:
 	Service() {};
 
